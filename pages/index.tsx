f6cb95216272d066b5c20103fa7f599a46801ac2
LOVAHE <lovahe@drypeng.io>
2024-01-23 22:56:31 +0900

    change index

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import FileUploader from '@components/FileUploader';
 const Home: React.FC = () => {
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Test</h1>
+      <h1 className="text-6xl font-bold mb-4">Test</h1>
       <FileUploader />
     </div>
   );
