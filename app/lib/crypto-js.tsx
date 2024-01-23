3b770703a256646a675409fc15d9ec029fdaf579
LOVAHE <lovahe@drypeng.io>
2024-01-23 14:03:11 +0900

    Update crypto-js.tsx

--- app/lib/crypto-js.tsx
+++ app/lib/crypto-js.tsx
@@ -1,12 +1,12 @@
 import CryptoJS from 'crypto-js';
 
 // Encrypt
-export const encryptFile = (fileContent, password) => {
+export const encryptFile = (fileContent: string, password: string) => {
     return CryptoJS.AES.encrypt(fileContent, password).toString();
 };
 
 // Decrypt
-export const decryptFile = (ciphertext, password) => {
+export const decryptFile = (ciphertext: string, password: string) => {
     const bytes = CryptoJS.AES.decrypt(ciphertext, password);
     return bytes.toString(CryptoJS.enc.Utf8);
 };
