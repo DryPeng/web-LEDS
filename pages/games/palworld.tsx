bab3d9c318938ac67fdb7d1519ccccdabf9e8a74
LOVAHE <lovahe@drypeng.io>
2024-01-23 18:33:36 +0900

    Update palworld.tsx

--- pages/games/palworld.tsx
+++ pages/games/palworld.tsx
@@ -6,6 +6,7 @@ const Pulworld: React.FC = () => {
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Pulworld</h1>
+      <FileUpload />
       <Footer />
     </div>
   );
