858cf0be7db0bf2a0a82922c3a5645da3f330779
LOVAHE <lovahe@drypeng.io>
2024-01-23 23:34:37 +0900

    change Footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import Footer from '@components/Footer';
 const Home: React.FC = () => {
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-6xl font-bold mb-4">Test</h1>
+      <h1 className="text-6xl font-bold pt-12 mb-4">Test</h1>
       <FileUploader />
 
       <Footer />
