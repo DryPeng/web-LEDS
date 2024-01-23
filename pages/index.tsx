2f4bddda4b22cbbd835cd7fbf85b578cc516d19d
LOVAHE <lovahe@drypeng.io>
2024-01-23 12:42:20 +0900

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import FileUploader from '@components/FileUploader';
+
+const Home: React.FC = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Test</h1>
+      <FileUploader />
+    </div>
+  );
+}
+
+export default Home;
