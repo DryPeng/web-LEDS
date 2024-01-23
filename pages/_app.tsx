9b5639c7ad0eac834ebe87f617ab69874cf25fe3
LOVAHE <lovahe@drypeng.io>
2024-01-23 23:12:49 +0900

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,19 @@
 import "../app/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+
+
+const App = ({ Component, pageProps }: AppProps) => {
+
+  return (
+    <>
+      <Head>
+      </Head>
+      <Component
+        {...pageProps}
+      />
+    </>
+  );
+};
 
 export default App;
