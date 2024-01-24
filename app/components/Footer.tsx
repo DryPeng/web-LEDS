e866e88f91f772ad6241c4f010f48b4a2fc01ea8
LOVAHE <lovahe@drypeng.io>
2024-01-24 19:07:51 +0900

    change copyright

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,7 +5,7 @@ export const Footer = () => {
     <footer className="bg-neutral-50 w-full py-16 bg-extra-strong">
       <div className="mt-8 py-6 border-t items-center justify-between px-10 py-4 mx-auto sm:px-6 row max-w-7xl">
         <div className="items-center gap-4 row">
-          <div className="text-sm text-light">&copy; {new Date().getFullYear()} Vaka</div>
+          <div className="text-sm text-light">&copy; {new Date().getFullYear()} DryPeng</div>
         </div>
       </div>
     </footer>
